70f0653329da77107c71be96ed59ad331131cb36
tejohnso <mail@tyler-johnson.ca>
2015-06-17 12:03:06 -0400

    Use node script instead of shell script

---
+++ run-test.js
@@ -0,0 +1,10 @@
+var testFile = process.argv[2],
+thisFilePath = require("path").dirname(process.argv[1]),
+shelljs = require("shelljs");
+
+if (!testFile) {console.log("No test file specified"); process.exit(1);}
+console.log("running test " + testFile + " from " + thisFilePath);
+
+shelljs.rm("./test-browserify.js");
+shelljs.exec("browserify " + testFile + " -o " + thisFilePath + "test-browserify.js");
+shelljs.exec(thisFilePath + "/node_modules/run-headless-chromium/run-headless-chromium.js --load-and-launch-app=" + thisFilePath);

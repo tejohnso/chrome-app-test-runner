13e8f80c6eb087925596e37cddf0ddaa82631e94
tejohnso <mail@tyler-johnson.ca>
2015-06-17 12:08:20 -0400

    Fix browserified file path

--- run-test.js
+++ run-test.js
@@ -6,7 +6,7 @@ if (!testFile) {console.log("No test file specified"); process.exit(1);}
 console.log("running test " + testFile + " from " + thisFilePath);
 
 shelljs.rm("./test-browserify.js");
-shelljs.exec("browserify " + testFile + " -o " + thisFilePath + "test-browserify.js");
+shelljs.exec("browserify " + testFile + " -o " + thisFilePath + "/test-browserify.js");
 
 if (process.platform === "win32") {
   shelljs.exec("google-chrome --load-and-launch-app=" + thisFilePath);

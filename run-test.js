8e86ce54ba0f6ec2de4f4242e6f5baa6b11cabb3
tejohnso <mail@tyler-johnson.ca>
2015-06-17 12:10:10 -0400

    Fix rm file path

--- run-test.js
+++ run-test.js
@@ -5,7 +5,7 @@ shelljs = require("shelljs");
 if (!testFile) {console.log("No test file specified"); process.exit(1);}
 console.log("running test " + testFile + " from " + thisFilePath);
 
-shelljs.rm("./test-browserify.js");
+shelljs.rm(thisFilePath + "/test-browserify.js");
 shelljs.exec("browserify " + testFile + " -o " + thisFilePath + "/test-browserify.js");
 
 if (process.platform === "win32") {

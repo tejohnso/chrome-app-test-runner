f99fc2a75961571496b2a9dc0dc06dc8918b1624
tejohnso <mail@tyler-johnson.ca>
2015-06-18 08:49:59 -0400

    Enable console logging to stderr

--- run-test.js
+++ run-test.js
@@ -9,4 +9,4 @@ shelljs.rm(thisFilePath + "/test-browserified.js");
 shelljs.rm("-rf", thisFilePath + "/temp-data-dir");
 shelljs.exec("browserify " + testFile + " -o " + thisFilePath + "/test-browserified.js");
 
-shelljs.exec("google-chrome --user-data-dir=" + thisFilePath + "/temp-data-dir" + " --load-and-launch-app=" + thisFilePath);
+shelljs.exec("google-chrome --enable-logging=stderr --log-level=0 --user-data-dir=" + thisFilePath + "/temp-data-dir" + " --load-and-launch-app=" + thisFilePath);

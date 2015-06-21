1a9be67d229c5a22c61c9c889ace6006e83bcc81
tejohnso <mail@tyler-johnson.ca>
2015-06-21 12:52:04 -0400

    Wait for mock server to start

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Takes a test file and browserifies it.
 The test file is expected to use node modules and mocha bdd tests.
 An optional mock http server can be specified with --mock-server=<full-path-to-js-file>
+The server must output a listening string indicating the port number when it is ready to receive requests.  For example: listening on 8888
 
 usage: node run-test.js <full-path-to-test-file> [<full-path-to-test-file> ...] [--mock-server=<full-path>]
--- run-test.js
+++ run-test.js
@@ -4,7 +4,6 @@ path = require("path"),
 thisFilePath = path.dirname(process.argv[1]),
 shelljs = require("shelljs"),
 spawn = require("child_process").spawn,
-serverProcess,
 regExp = /[^"]*"(.*)", source: chrome-extension/,
 EOL = require("os").EOL;
 
@@ -20,53 +19,67 @@ function browserifyTestFile(file) {
 }
 
 function runTest(fullFilePath) {
-  var childProcess;
-
   if (!fullFilePath) {return;}
   cleanLaunchEnvironment();
   browserifyTestFile(fullFilePath);
-  startServer();
-  console.log(EOL + "Running test " + fullFilePath);
-  childProcess = spawn("google-chrome", ["--enable-logging=stderr", "--log-level=0", "--user-data-dir=" + path.join(thisFilePath, "temp-data-dir"), " --load-and-launch-app=" + thisFilePath]);
+  startServer()
+  .then(function(serverProcess) {
+    var chromeProcess;
 
-  childProcess.on("close", function(code) {
-    console.log("Closed child process");
-    stopServer();
-    runTest(testFiles.shift());
-  });
+    console.log(EOL + "Running test " + fullFilePath);
 
-  childProcess.stderr.on("data", function(data) {
-    var logOutput = regExp.exec(data.toString());
-    if (!logOutput) {return;}
-    console.log(logOutput[1]);
-    if (logOutput[1].indexOf("All tests completed!0") > -1) {
-      return setTimeout(function() {childProcess.kill();}, 500);
-    }
-    if (logOutput[1].indexOf("All tests completed!") > -1) {
-      testFiles = [];
-    }
-  });
+    chromeProcess = spawn("google-chrome", 
+    ["--enable-logging=stderr",
+    "--log-level=0",
+    "--user-data-dir=" + path.join(thisFilePath, "temp-data-dir"),
+    " --load-and-launch-app=" + thisFilePath]);
 
-  childProcess.stdout.on("data", function(data) {
-    console.log("Chrome stdout: " + data);
+    chromeProcess.on("close", function(code) {
+      console.log("Closed child process");
+      stopServer(serverProcess);
+      runTest(testFiles.shift());
+    });
+
+    chromeProcess.stderr.on("data", function(data) {
+      var logOutput = regExp.exec(data.toString());
+      if (!logOutput) {return;}
+      console.log(logOutput[1]);
+      if (logOutput[1].indexOf("All tests completed!0") > -1) {
+        return setTimeout(function() {chromeProcess.kill();}, 500);
+      }
+      if (logOutput[1].indexOf("All tests completed!") > -1) {
+        testFiles = [];
+      }
+    });
+
+    chromeProcess.stdout.on("data", function(data) {
+      console.log("Chrome stdout: " + data);
+    });
   });
-}
+};
 
 function startServer() {
-  if (!cliOptions.hasOwnProperty("mock-server")) {return;}
-  console.log(EOL + "Starting server " + cliOptions["mock-server"]);
-  serverProcess = spawn("node", [cliOptions["mock-server"]]);
-  console.log("Server pid: " + serverProcess.pid);
-  serverProcess.stdout.on("data", function(data) {
-    console.log("Mock server: " + data);
-  });
-  serverProcess.stderr.on("data", function(data) {
-    console.log("Mock server err: " + data);
+  return new Promise(function(resolve, reject) {
+    var serverProcess;
+    if (!cliOptions.hasOwnProperty("mock-server")) {resolve();}
+
+    console.log(EOL + "Starting server " + cliOptions["mock-server"]);
+    serverProcess = spawn("node", [cliOptions["mock-server"]]);
+    console.log("Server pid: " + serverProcess.pid);
+    serverProcess.stdout.on("data", function(data) {
+      console.log("Mock server: " + data);
+      if (/listening on [\d]*/i.test(data.toString())) {
+        resolve(serverProcess);
+      }
+    });
+    serverProcess.stderr.on("data", function(data) {
+      console.log("Mock server err: " + data);
+    });
   });
 }
 
-function stopServer() {
-  if (!serverProcess) {return;}
+function stopServer(process) {
+  if (!process) {return;}
   console.log("Stopping server");
-  serverProcess.kill();
+  process.kill();
 }
